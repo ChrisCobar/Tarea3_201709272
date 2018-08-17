29f8d9e0b9401f77697387b48bcca106574bb5ec
cris <cris@192.168.0.33>
2018-08-16 21:27:35 -0600

    estoy trabajando en mi tarea

--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -7,12 +7,11 @@ import java.util.Scanner;
 public class MenuPrincipal {
     
     public static String [] usuarios; //declaracion de un vector llamado usuarios
+    public static int contadorNum; // variable que captura el numero entero ingresado por el usuario
+    public static String caracteresNum;
+    public static int numeroElecto;
     
-    public static void main(String[] args) { //metodo principal donde inicia la ejecucion de todo programa en JAVA
-        
-        MenuPrincipal.menuprincipal(); //llamada al metodo que contiene al menu principal
-           
-    }
+    
     
     //......................................................................................................................................
     //......................................................................................................................................
@@ -25,7 +24,7 @@ public class MenuPrincipal {
         Scanner menu = new Scanner(System.in);  //se instancia un objeto de tipo scanner para poder capturar la opcion del usuario
         
         System.out.println("----------------------------------------------------"); // se imprime en consola el menu 
-        System.out.println("----------------------------------------------------");
+        System.out.println("-------------------------MENU-----------------------");
         System.out.println("1. Usuarios");
         System.out.println("2. Contador de digitos repetidos");
         System.out.println("3. Tres numeros de mayor a menor");
@@ -42,6 +41,7 @@ public class MenuPrincipal {
             break;
             
             case 2:
+                MenuPrincipal.contadorDigitos();
             break;
             
             case 3:
@@ -60,7 +60,7 @@ public class MenuPrincipal {
           
         }
         }catch(Exception e){ // permite la captura de cualquier error y nos regresa al menu principal
-            System.out.println("ERROR, opcion invalida, intente otra vez");
+           System.out.println("ERROR, opcion invalida, intente otra vez");
             MenuPrincipal.menuprincipal();
         }
     }
@@ -72,7 +72,7 @@ public class MenuPrincipal {
         int teclado; //declaracion de una variable para evaluar el swich
         Scanner menu = new Scanner(System.in);  //se instancia un onjeto de tipo scanner para poder capturar la opcion que desea el usuario
         
-        System.out.println("OPCION USUARIOS");
+        System.out.println("-----------------OPCION USUARIOS--------------------");;
         System.out.println("1. Ingresar usuario");
         System.out.println("2. Mostrar usuarios ascendente");
         System.out.println("3. Mostrar usuarios descendente");
@@ -87,7 +87,8 @@ public class MenuPrincipal {
             break;
             
             case 2:
-                System.out.println("ordenados de iltimo a primero: "); // ya que el vector es pequeño, se imprime desde la ultima posicion a la primera
+                System.out.println("---------------------------------------------------");
+                System.out.println("ordenados de ultimo a primero: "); // ya que el vector es pequeño, se imprime desde la ultima posicion a la primera
                 System.out.println("5: "+usuarios[4]);
                 System.out.println("4: "+usuarios[3]);
                 System.out.println("3: "+usuarios[2]);
@@ -98,6 +99,7 @@ public class MenuPrincipal {
             break;
             
             case 3:
+                System.out.println("---------------------------------------------------");
                 System.out.println("ordenados de primero al ultimo: "); // ya que el vector es pequeño, se imprime desde la primera a la ultima
                 System.out.println("1: "+usuarios[0]);
                 System.out.println("2: "+usuarios[1]);
@@ -131,6 +133,7 @@ public class MenuPrincipal {
          usuarios = new String[5]; //creacion del vector que guardara los 5 usuarios
          Scanner teclado = new Scanner(System.in);
          String user;
+         System.out.println("---------------------------------------------------");
          System.out.println("Bienvenido a ingreso de usuarios");// mensaje en pantalla para el usuario
          System.out.println("Ingrese el usuario 1");
          user = teclado.next();  // lee lo que el usuario ingresa y lo guarda en la variable user
@@ -176,7 +179,181 @@ public class MenuPrincipal {
      
     //......................................................................................................................................
     //......................................................................................................................................
+    public static void contadorDigitos(){
+        
+        int teclado; //declaracion de una variable para evaluar el swich
+        Scanner menu = new Scanner(System.in);  //se instancia un onjeto de tipo scanner para poder capturar la opcion que desea el usuario
+        
     
-     
+        System.out.println("CONTADOR DE DIGITOS");
+        System.out.println("1. Ingresar numero");
+        System.out.println("2. Ingresar numero a buscar");
+        System.out.println("3. mostrar numero de digitos");
+        System.out.println("4. Menu principal");
+        teclado = menu.nextInt();
+        System.out.println("----------------------------------------------------");
+        switch(teclado){
+            
+            case 1:
+                System.out.println("----------------------------------------------------");
+                System.out.println("Ingrese Numero: ");
+                contadorNum = menu.nextInt(); // se guarda la variable ingresada por el usuario en una variable global
+                System.out.println("numero guardado");
+                System.out.println("----------------------------------------------------");
+                MenuPrincipal.contadorDigitos();
+                
+                break;
+            case 2:
+                System.out.println("----------------------------------------------------");
+                System.out.println("Ingrese un numero entero entre 0 a 9");
+                teclado = menu.nextInt();
+                numeroElecto = teclado;
+                System.out.println("numero guardado");
+                System.out.println("----------------------------------------------------");
+                MenuPrincipal.contadorDigitos();
+                break;
+            case 3:
+                
+                MenuPrincipal.opcion3Contador();
+                MenuPrincipal.contadorDigitos();
+                
+            case 4:
+                MenuPrincipal.menuprincipal(); // metodo de la clase menu que nos permite regresar al menu principal
+                break;
+            
+            default:
+                System.out.println("Esta opcion no existe, elegir un numero del 1 al 4"); // como la opcion ingresada es un numero invalido, regresa al menu 
+                MenuPrincipal.contadorDigitos();
+                break;
+        }
     
+    }
+    //......................................................................................................................................
+    //......................................................................................................................................
+    public static void opcion3Contador(){
+    
+        String numero; // se declara una variable de tipo cadena
+        numero = Integer.toString(contadorNum); //se utiliza la variable de tipo cadena para convertir el numero a cadena
+        
+        int contador=0; //contador que servira para registrar cuantas veces se repite el numero
+        char letra1;
+        char [] vector;
+        
+                 vector = numero.toCharArray(); // llamamos al metodo que nos optiene todos los caracteres de la cadena
+                
+                switch(numeroElecto){ // se evalua el numero ingresado en la opcion 2 (numero a buscar)
+                    case 0:
+                        for (int i = 0; i < vector.length; i++) { //ciclo que recorre el vector en busca de las posiciones donde es igual al caracter
+                            letra1 = vector[i];
+                             if (letra1 == '0') {
+                                contador++;                               
+                            }      
+                        }
+                        System.out.println("EL numero "+numeroElecto+" aparece:");
+                        System.out.println(+contador+ " veces");
+                        break;
+                    case 1:
+                        for (int i = 0; i < vector.length; i++) {
+                            letra1 = vector[i];
+                             if (vector[i] == '1') {
+                                contador++;                               
+                            }      
+                        }
+                        System.out.println("EL numero "+numeroElecto+" aparece:");
+                        System.out.println(+contador+ " veces");
+                        break;
+                    case 2:
+                        for (int i = 0; i < vector.length; i++) {
+                            letra1 = vector[i];
+                             if (letra1 == '2') {
+                                contador++;                               
+                            }      
+                        }
+                        System.out.println("EL numero "+numeroElecto+" aparece:");
+                        System.out.println(+contador+ " veces");
+                        break;
+                    case 3:
+                        for (int i = 0; i < vector.length; i++) {
+                            letra1 = vector[i];
+                             if (letra1 == '3') {
+                                contador++;                               
+                            }      
+                        }
+                        System.out.println("EL numero "+numeroElecto+" aparece:");
+                        System.out.println(+contador+ " veces");
+                        break;
+                    case 4:
+                        for (int i = 0; i < vector.length; i++) {
+                            letra1 = vector[i];
+                             if (letra1 == '4') {
+                                contador++;                               
+                            }      
+                        }
+                        System.out.println("EL numero "+numeroElecto+" aparece:");
+                        System.out.println(+contador+ " veces");
+                        break;
+                    case 5:
+                        for (int i = 0; i < vector.length; i++) {
+                            letra1 = vector[i];
+                             if (letra1 == '5') {
+                                contador++;                               
+                            }      
+                        }
+                        System.out.println("EL numero "+numeroElecto+" aparece:");
+                        System.out.println(+contador+ " veces");
+                        break;
+                    case 6:
+                        for (int i = 0; i < vector.length; i++) {
+                            letra1 = vector[i];
+                             if (letra1 == '6') {
+                                contador++;                               
+                            }      
+                        }
+                        System.out.println("EL numero "+numeroElecto+" aparece:");
+                        System.out.println(+contador+ " veces");
+                        break;
+                    case 7:
+                        for (int i = 0; i < vector.length; i++) {
+                            letra1 = vector[i];
+                             if (letra1 == '7') {
+                                contador++;                               
+                            }      
+                        }
+                        System.out.println("EL numero "+numeroElecto+" aparece:");
+                        System.out.println(+contador+ " veces");
+                        break;
+                    case 8:
+                        for (int i = 0; i < vector.length; i++) {
+                            letra1 = vector[i];
+                             if (letra1 == '8') {
+                                contador++;                               
+                            }      
+                        }
+                        System.out.println("EL numero "+numeroElecto+" aparece:");
+                        System.out.println(+contador+ " veces");
+                        break;
+                    case 9:
+                        for (int i = 0; i < vector.length; i++) {
+                            letra1 = vector[i];
+                             if (letra1 == '9') {
+                                contador++;                               
+                            }      
+                        }
+                        System.out.println("EL numero "+numeroElecto+" aparece:");
+                        System.out.println(+contador+ " veces");
+                        break;
+                    default:
+                        System.out.println("error, intente otra vez");
+                        break; 
+                }            
+    }
+    
+    //......................................................................................................................................
+    //......................................................................................................................................
+  
+    public static void main(String[] args) { //metodo principal donde inicia la ejecucion de todo programa en JAVA
+        
+        MenuPrincipal.menuprincipal(); //llamada al metodo que contiene al menu principal
+           
+    }
 }
